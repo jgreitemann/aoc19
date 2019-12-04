fec882fee931b33a30b5d708dd46413476fb69f6
Jonas Greitemann <j.greitemann@lmu.de>
2019-12-04 23:12:31 +0100

    Day 2 -- Part 2

--- day02/intcode.cpp
+++ day02/intcode.cpp
@@ -1,4 +1,7 @@
+#include <range/v3/view/cartesian_product.hpp>
+#include <range/v3/view/filter.hpp>
 #include <range/v3/view/getlines.hpp>
+#include <range/v3/view/iota.hpp>
 #include <range/v3/view/transform.hpp>
 
 #include <iostream>
@@ -13,30 +16,47 @@ int main()
     auto memory = getlines(std::cin, ',') |
                   views::transform([](auto str) { return std::stoi(str); }) |
                   to<std::vector<int>>();
-    memory[1] = 12;
-    memory[2] = 02;
 
-    auto process = [&memory](auto & it) {
-        int opcode = *(it++);
-        switch (opcode) {
-            case 1:
-                memory[it[2]] = memory[it[0]] + memory[it[1]];
-                it += 3;
-                return true;
-            case 2:
-                memory[it[2]] = memory[it[0]] * memory[it[1]];
-                it += 3;
-                return true;
-            case 99:
-                return false;
-            default:
-                throw std::runtime_error("invalid opcode");
-                return false;
+    auto run = [](int noun, int verb, std::vector<int> memory) {
+        auto process = [&memory](auto & it) {
+            int opcode = *(it++);
+            switch (opcode) {
+                case 1:
+                    memory[it[2]] = memory[it[0]] + memory[it[1]];
+                    it += 3;
+                    return true;
+                case 2:
+                    memory[it[2]] = memory[it[0]] * memory[it[1]];
+                    it += 3;
+                    return true;
+                case 99:
+                    return false;
+                default:
+                    throw std::runtime_error("invalid opcode");
+                    return false;
+            }
+        };
+
+        memory[1] = noun;
+        memory[2] = verb;
+        for (auto ip = memory.begin(); process(ip);) {
         }
+        return memory[0];
     };
 
-    for (auto ip = memory.begin(); process(ip);) {
-    }
+    std::cout << "Part 1: " << run(12, 02, memory) << '\n';
 
-    std::cout << memory[0] << '\n';
+    auto find_inputs = [&](int desired_result) {
+        for (auto [noun, verb] : views::cartesian_product(views::iota(0, 100),
+                                                          views::iota(0, 100)) |
+                                     views::filter([&](auto pair) {
+                                         auto [noun, verb] = pair;
+                                         return run(noun, verb, memory) ==
+                                                desired_result;
+                                     }))
+            return 100 * noun + verb;
+        throw std::runtime_error(
+            "could not find inputs matching desired result");
+    };
+    std::cout << "Part 2: " << find_inputs(19690720) << '\n';
 }
